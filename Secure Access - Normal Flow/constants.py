8a3849e7a852c09a991d18fdad675380a86882a5
Sriraam-K <116096802+Sriraam-K@users.noreply.github.com>
2023-06-27 20:33:09 +0530

    Secure Access - Normal Flow

---
+++ Secure Access - Normal Flow/constants.py
@@ -0,0 +1,10 @@
+# This file holds the host address and port numbers for the ECU and Trust Center Server and verified MAC addresses
+# of verified testers
+
+ECU_HOST = 'ECU_IP_ADDRESS' # Set this to Computer A's IPv4 Address
+ECU_PORT = 9002
+
+TRUST_CENTER_HOST = 'TRUST_CENTRE_IP_ADDRESS' # Set this to Computer B's IPv4 Address
+TRUST_CENTER_PORT = 8001
+
+TRUSTED_MAC_LIST  = ["TRUSTED_TESTER_MAC_ADDRESS"] # Add the MAC address of the PCs running tester program
